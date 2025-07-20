a8a921a504a31b2f919b7f5ed6516bb26a918c98
andreyfcs <andrey_cs@hotmail.com>
2025-07-20 13:26:05 -0300

    Adicionando troca de Telas

--- script.js
+++ script.js
@@ -91,17 +91,74 @@ const flappyBird = {
     }
 }
 
+// [mensagemGetReady
+const mensagemGetReady = {
+    spriteX: 134,
+    spriteY: 0,
+    largura: 174,
+    altura: 152,
+    x: (canvas.width / 2) - (174 / 2),
+    y: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.spriteX, mensagemGetReady.spriteY,
+            mensagemGetReady.largura, mensagemGetReady.altura,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.largura, mensagemGetReady.altura,
+        );
+    }
+}
+
+// [Telas]
+let telaAtiva = {};
+function mudaParaTela(novaTela){
+    telaAtiva = novaTela;
+}
+
+const Telas = {
+    INICIO: {
+        desenha() {       
+            planoDeFundo.desenha();
+            flappyBird.desenha();
+            chao.desenha();
+            mensagemGetReady.desenha();
+        },
+        click(){
+            mudaParaTela(Telas.JOGO);
+        },
+        atualiza() {
+
+        }
+      }
+    };
+
+    Telas.JOGO = {
+        desenha() {
+        planoDeFundo.desenha();
+        flappyBird.desenha();
+        chao.desenha();
+
+        },
+        atualiza() {
+        flappyBird.atualiza();
+        }
+    };
+
+
 // "Onde o Jogo Acontece"
 function loop() {
     
-    planoDeFundo.desenha();
-    flappyBird.atualiza();
-    flappyBird.desenha();
-    chao.desenha();
-    
-    flappyBird.y += 1;    
-
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function(){
+    if(telaAtiva.click) {
+        telaAtiva.click();
+    }
+});
+
+mudaParaTela(Telas.INICIO);
 loop();
